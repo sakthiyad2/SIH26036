9b3100c233ff701f765beec3c8faa002620aea49
Sakthiya D <sakthiya.d.aids29@princedrkvasudevan.com>
2026-09-06 10:23:16 +0530

    Fix GitHub Pages base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/SIH26036/',
+
   server: {
     proxy: {
       "/api": {
